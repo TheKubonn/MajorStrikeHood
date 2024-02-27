c31739fc472bc3ac6daffc79d69befddd43e9762
F1ZZ <TheKubonn@hotmail.com>
2024-02-27 15:20:39 +0100

    Beginning of journey

---
+++ Source/MajorStrikeHood/Public/SCharacter.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class MAJORSTRIKEHOOD_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr <UCameraComponent> CameraComp;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr <USpringArmComponent> SpringArmComp;
+
+private:	
+
+
+};
